4d7aa55c36058ba4bbd02950e506ef9aef1608e6
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-17 13:23:09 +1300

    Refactored code into 'Game' class

--- source/src/main.cpp
+++ source/src/main.cpp
@@ -15,7 +15,7 @@ namespace {
   const float STEP = 1.0/1000.0;
 
   const float WINKY_MASS = 1.0;
-  const btVector3 GRAVITY(0,-3,0);
+  const btVector3 GRAVITY(0,-20,0);
 }
 
 class MotionState : public btMotionState
@@ -65,122 +65,162 @@ private:
    }
 };
 
+class Game : public IEventReceiver
+{
+public:
+  Game() {
+    this->device = createDevice(
+      video::EDT_OPENGL,
+      dimension2d<u32>(1024, 768),
+      16,
+      false,
+      false,
+      false,
+      this);
 
-int main() {
-  IrrlichtDevice *device = createDevice(
-    video::EDT_OPENGL,
-    dimension2d<u32>(1024, 768),
-    16,
-    false,
-    false,
-    false,
-    0);
+    if (!device) return;
+
+    device->setWindowCaption(L"Powerball");
+
+    this->driver = device->getVideoDriver();
+    this->smgr = device->getSceneManager();
+
+    btBroadphaseInterface *broadPhase = new btAxisSweep3(btVector3(-1000, -1000, -1000), btVector3(1000, 1000, 1000));
+    btDefaultCollisionConfiguration *collisionConfiguration = new btDefaultCollisionConfiguration();
+    btCollisionDispatcher *dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    btSequentialImpulseConstraintSolver *solver = new btSequentialImpulseConstraintSolver();
+    world = new btDiscreteDynamicsWorld(dispatcher, broadPhase, solver, collisionConfiguration);
+
+    world->setGravity(GRAVITY);
+
+    this->winkyShape = new btSphereShape(1);
+
+    IAnimatedMesh *mesh = smgr->getMesh("/Users/dylanmckay/Desktop/winky.3ds");
+    IAnimatedMesh *groundMesh = smgr->getMesh("/Users/dylanmckay/Desktop/map.3ds");
+
+    if (!mesh || !groundMesh) {
+      device->drop();
+      return;
+    }
+
+    winkyNode = smgr->addAnimatedMeshSceneNode(mesh);
+    winkyNode->setPosition(vector3df(0,5,0));
+
+    //winkyNode->setMaterialTexture(0, driver->getTexture("/Users/dylanmckay/Desktop/winky.bmp"));
+
+    groundNode = smgr->addAnimatedMeshSceneNode(groundMesh);
+
+    camera = smgr->addCameraSceneNode();
+
+    ILightSceneNode *light = smgr->addLightSceneNode(nullptr, vector3df(0,10,0));
+
+    camera->setPosition(vector3df(13,7,-13));
+    camera->setTarget(vector3df(0,0,0));
+
+    groundShape = new btStaticPlaneShape(btVector3(0,1,0), 1);
+    btMotionState *groundState =
+      new btDefaultMotionState(btTransform(
+          btQuaternion(0,0,0,1),
+          btVector3(0, -1, 0)));
+
+    btRigidBody::btRigidBodyConstructionInfo groundCI(0, groundState, groundShape, btVector3(0,0,0));
+    groundBody = new btRigidBody(groundCI);
+
+
+    btMotionState *winkyState = new MotionState(winkyNode);
+
+    btVector3 winkyInertia(0,0,0);
+    winkyShape->calculateLocalInertia(WINKY_MASS, winkyInertia);
+    btRigidBody::btRigidBodyConstructionInfo winkyRigidBodyCI(WINKY_MASS,
+                                                              winkyState,
+                                                              winkyShape,
+                                                              winkyInertia);
+    winkyRigidBodyCI.m_friction = 50.0;
+
+    winkyBody = new btRigidBody(winkyRigidBodyCI);
+
+    world->addRigidBody(winkyBody);
+    world->addRigidBody(groundBody);
+  }
+
+  ~Game() {
+    device->drop();
+
+    delete winkyBody->getMotionState();
+    delete groundBody->getMotionState();
+
+    delete winkyBody;
+    delete groundBody;
+
+    delete winkyShape;
+    delete groundShape;
+  }
+
+  void run() {
+    while (device->run()) {
+      world->stepSimulation(STEP, 10);
+
+      //camera->setPosition(winkyNode->getPosition() - vector3df(0,2,1));
+      camera->setTarget(winkyNode->getPosition());
+      driver->beginScene(true, true, SColor(255,100,101,140));
+
+      smgr->drawAll();
+
+      driver->endScene();
+    }
+  }
+
+  bool OnEvent(const SEvent &event) {
+    switch (event.EventType) {
+    case EET_KEY_INPUT_EVENT:
+      return keyEvent(event.KeyInput);
+    default:
+      return false;
+    }
+  }
+
+  bool keyEvent(const SEvent::SKeyInput &event) {
+    if (!event.PressedDown) return false;
+
+    double accelerationSize = 50.0;
+
+    btVector3 forwardVector = btVector3(1,1,1);
+    btVector3 leftVector = btVector3(1,0,0);
+
+    switch (event.Key) {
+    case KEY_UP:
+      accelerate(accelerationSize * forwardVector);
+      return true;
+    case KEY_DOWN:
+      accelerate(accelerationSize * -forwardVector);
+      return true;
+    default:
+      return false;
+    }
+  }
+
+  void accelerate(btVector3 vec) {
+    this->winkyBody->applyTorqueImpulse(vec);
+  }
+
+private:
+  IrrlichtDevice *device;
+  IVideoDriver *driver;
+  ISceneManager *smgr;
+  ICameraSceneNode *camera;
+  ISceneNode *winkyNode;
+  ISceneNode *groundNode;
 
   btDiscreteDynamicsWorld *world;
-  btBroadphaseInterface *broadPhase = new btAxisSweep3(btVector3(-1000, -1000, -1000), btVector3(1000, 1000, 1000));
-  btDefaultCollisionConfiguration *collisionConfiguration = new btDefaultCollisionConfiguration();
-  btCollisionDispatcher *dispatcher = new btCollisionDispatcher(collisionConfiguration);
-  btSequentialImpulseConstraintSolver *solver = new btSequentialImpulseConstraintSolver();
-  world = new btDiscreteDynamicsWorld(dispatcher, broadPhase, solver, collisionConfiguration);
 
-  world->setGravity(GRAVITY);
+  btCollisionShape *winkyShape;
+  btCollisionShape *groundShape;
+  btRigidBody *winkyBody;
+  btRigidBody *groundBody;
+};
 
-  btCollisionShape *winkyShape = new btSphereShape(1);
-
-  if (!device) return 1;
-
-  device->setWindowCaption(L"Powerball");
-  IVideoDriver *driver = device->getVideoDriver();
-  ISceneManager *smgr = device->getSceneManager();
-  IGUIEnvironment *gui = device->getGUIEnvironment();
-
-  IAnimatedMesh *mesh = smgr->getMesh("/Users/dylanmckay/winky.obj");
-  IAnimatedMesh *groundMesh = smgr->getMesh("/Users/dylanmckay/Desktop/map.3ds");
-
-  if (!mesh || !groundMesh) {
-    device->drop();
-    return 1;
-  }
-
-  IAnimatedMeshSceneNode *winkyNode = smgr->addAnimatedMeshSceneNode(mesh);
-
-  winkyNode->setPosition(vector3df(0,5,0));
-
-  IAnimatedMeshSceneNode *groundNode = smgr->addAnimatedMeshSceneNode(groundMesh);
-
-  SKeyMap keyMap[8];
-  keyMap[0].Action = EKA_MOVE_FORWARD;
-  keyMap[0].KeyCode = KEY_UP;
-  keyMap[1].Action = EKA_MOVE_FORWARD;
-  keyMap[1].KeyCode = KEY_KEY_W;
-
-  keyMap[2].Action = EKA_MOVE_BACKWARD;
-  keyMap[2].KeyCode = KEY_DOWN;
-  keyMap[3].Action = EKA_MOVE_BACKWARD;
-  keyMap[3].KeyCode = KEY_KEY_S;
-
-  keyMap[4].Action = EKA_STRAFE_LEFT;
-  keyMap[4].KeyCode = KEY_LEFT;
-  keyMap[5].Action = EKA_STRAFE_LEFT;
-  keyMap[5].KeyCode = KEY_KEY_A;
-
-  keyMap[6].Action = EKA_STRAFE_RIGHT;
-  keyMap[6].KeyCode = KEY_RIGHT;
-  keyMap[7].Action = EKA_STRAFE_RIGHT;
-  keyMap[7].KeyCode = KEY_KEY_D;
-
-  //ICameraSceneNode *camera = smgr->addCameraSceneNodeFPS(nullptr, 100.0, 0.1, -1, keyMap, 8);
-  ICameraSceneNode *camera = smgr->addCameraSceneNode();
-
-  ILightSceneNode *light = smgr->addLightSceneNode(nullptr, vector3df(0,10,0));
-
-  camera->setPosition(vector3df(13,7,-13));
-  camera->setTarget(vector3df(0,0,0));
-
-  btCollisionShape *groundShape = new btStaticPlaneShape(btVector3(0,1,0), 1);
-  btDefaultMotionState *groundState =
-    new btDefaultMotionState(btTransform(
-        btQuaternion(0,0,0,1),
-        btVector3(0, -1, 0)));
-
-  btRigidBody::btRigidBodyConstructionInfo groundCI(0, groundState, groundShape, btVector3(0,0,0));
-  btRigidBody *groundBody = new btRigidBody(groundCI);
-
-
-  MotionState *winkyState = new MotionState(winkyNode);
-
-  btVector3 winkyInertia(0,0,0);
-  winkyShape->calculateLocalInertia(WINKY_MASS, winkyInertia);
-  btRigidBody::btRigidBodyConstructionInfo winkyRigidBodyCI(WINKY_MASS,
-                                                            winkyState,
-                                                            winkyShape,
-                                                            winkyInertia);
-  btRigidBody *winkyBody = new btRigidBody(winkyRigidBodyCI);
-
-  world->addRigidBody(winkyBody);
-  world->addRigidBody(groundBody);
-
-  while (device->run()) {
-    world->stepSimulation(STEP, 10);
-
-    //camera->setPosition(winkyNode->getPosition() - vector3df(0,2,1));
-    camera->setTarget(winkyNode->getPosition());
-    driver->beginScene(true, true, SColor(255,100,101,140));
-
-    smgr->drawAll();
-    gui->drawAll();
-
-    driver->endScene();
-  }
-
-  device->drop();
-
-  delete winkyBody;
-  delete groundBody;
-  delete winkyState;
-  delete groundState;
-  delete winkyShape;
-  delete groundShape;
-
+int main() {
+  Game game;
+  game.run();
   return 0;
 }
